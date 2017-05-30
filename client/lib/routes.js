b4d199e780264c2b499f3b173ab8dbd5cf229aed
kndllbrwn <kndllbrwn1914@gmail.com>
2017-05-29 21:25:43 -0400

    Added login and signup templates

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -4,3 +4,17 @@ FlowRouter.route('/', {
     BlazeLayout.render('fullLayout', { main: 'heroUnit' })
   }
 })
+
+FlowRouter.route('/signup', {
+  name: 'signup',
+  action() {
+    BlazeLayout.render('fullLayout', { main: 'signup' })
+  }
+})
+
+FlowRouter.route('/login', {
+  name: 'login',
+  action() {
+    BlazeLayout.render('fullLayout', { main: 'login' })
+  }
+})
