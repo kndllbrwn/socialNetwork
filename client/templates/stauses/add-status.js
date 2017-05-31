473ef2642fd97ea61b703ae8f77758a2de265c24
kndllbrwn <kndllbrwn1914@gmail.com>
2017-05-31 12:13:52 -0400

    Updated status UI and validation

---
+++ client/templates/stauses/add-status.js
@@ -0,0 +1,28 @@
+Template.addStatus.onCreated(() => {
+  /** Initialize the error session variable */
+  Session.set('error', '');
+});
+
+Template.addStatus.events({
+  'submit form' (event) {
+    event.preventDefault();
+    let status = event.target.status.value;
+    
+    if (!_.isEmpty(status)) {
+      // call method
+      event.target.status.value = '';
+      Session.set('error', '');
+    } else {
+      Session.set('error', 'The status cannot be blank');
+    }
+  }
+});
+
+Template.addStatus.helpers({
+  /**
+   * Check to see if we have an error, and if we do return a message
+   */
+  'haveErrors' () {
+    if (Session.get('error')) { return Session.get('error'); }
+  }
+});
